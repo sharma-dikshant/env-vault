69e0d3ada24c11311a51b2e1064183befd4bcade
Vidit-GKMIT <vidit@gkmit.co>
2025-10-14 18:39:52 +0530

    fix:update route

--- controllers/project.controller.js
+++ controllers/project.controller.js
@@ -21,23 +21,11 @@ export const createProject = async (req, res, next) => {
       return res.status(400).json({ message: "Project name is required" });
     }
 
-    const project = await Project.create({ name, user, secret: [] });
-
-    if (Array.isArray(secret) && secret.length > 0) {
-      for (let i = 0; i < secret.length; i++) {
-        const s = secret[i];
-        if (s.key && !s.value) {
-          return res.status(400).json({ message: "Value is required if key is provided" });
-        }
-        if (s.value && !s.key) {
-          return res.status(400).json({ message: "Key is required if value is provided" });
-        }
-        project.secret.push({ key: s.key, value: s.value });
-      }
-
-      await project.save();
+    if(!user){
+      return res.status(400).json({ message: "User ID is required" });
     }
 
+    const project = await Project.create({ name, user, secret});
     res.status(201).json({ message: "Project created successfully", project });
   } catch (err) {
     res.status(500).json({ message: "Internal Server Error", error: err.message });
@@ -50,42 +38,15 @@ export const updateProject = async (req, res, next) => {
     const { projectId } = req.params;
     const { userId, secret, name } = req.body;
 
-    const project = await Project.findOne({ _id: projectId, user: userId });
-    if (!project) {
+    const doc = await Project.findOne({ _id: projectId, user: userId, isActive: true });
+    if(!doc){
       return res.status(404).json({ message: "Project not found" });
     }
-
-    if (!Array.isArray(secret) || secret.length === 0) {
-      project.secret = [];
-    }
-
-    if (Array.isArray(secret) && secret.length > 0) {
-      secret.forEach(s => {
-        if (s.key && !s.value) {
-          throw new Error("Value is required if key is provided");
-        }
-        if (s.value && !s.key) {
-          throw new Error("Key is required if value is provided");
-        }
-
-        if ('_id' in s) {
-          const subdoc = project.secret.id(s._id);
-          if (subdoc) {
-            subdoc.key = s.key;
-            subdoc.value = s.value;
-          }
-        } else {
-          project.secret.push({ key: s.key, value: s.value });
-        }
-      });
-    }
-
     if (!name) {
       return res.status(400).json({ message: "Project name is required" });
     }
-    project.name = name;
+    let project = await Project.findByIdAndUpdate(projectId, { name,secret }, { new: true });
     await project.save();
-
     res.status(200).json({ message: "Project updated successfully", project });
   } catch (err) {
     res.status(500).json({ message: "Internal Server Error", error: err.message });
