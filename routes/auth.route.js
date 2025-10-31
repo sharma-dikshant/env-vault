202d64ce47628783eeada9d947e44959c8d5de21
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-10-31 11:04:47 +0530

    feat: add /me route

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -5,12 +5,19 @@ import {
   signup,
   loginWithGoogle,
 } from "../controllers/auth.controller.js";
+import { protect } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router
   .post("/login", loginWithPassword)
   .post("/signup", signup)
   .post("/logout", logout)
-  .post("/google", loginWithGoogle);
+  .post("/google", loginWithGoogle)
+  .get("/me", protect, (req, res) => {
+    return res.status(200).json({
+      message: "user fetched",
+      user: req.user,
+    });
+  });
 
 export default router;
