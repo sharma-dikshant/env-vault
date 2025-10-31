ed44cb134c07bd2f17e005429accbfeb739b7767
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-10-31 11:04:12 +0530

    fix: cors

--- app.js
+++ app.js
@@ -2,28 +2,27 @@ import express from "express";
 import morgan from "morgan";
 import authRouter from "./routes/auth.route.js";
 import projectRouter from "./routes/project.route.js";
+import cors from "cors";
 
 const app = express();
 
+const allowedOrigins = ["http://localhost:5678", "http://localhost:5173"];
+
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 app.use("/auth", authRouter);
 app.use("/projects", projectRouter);
-app.use("/", (req, res) => {
-  res.status(200).json({
-    message: "success",
-  });
-});
 
-app.get("/", () => {
-  console.log("welcome");
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "welcome" });
 });
 
 app.use((err, req, res, next) => {
   return res.status(500).json({
-    message: err.message
-  })
-})
+    message: err.message,
+  });
+});
 
 export default app;
